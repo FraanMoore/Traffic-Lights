ad87d90132485a0b1eb0b9ab180e4607ffecd1b4
FraanMoore <fraanmoore@gmail.com>
2023-02-04 22:55:13 -0300

    agregando el respositorio

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import { useState } from 'react'
+
+
+
+const TrafficLight = () => {
+  const [color, setColor] = useState("");
+
+  return (
+    <div className='semaforo'>
+      <div className="btn-group-vertical rounded-circle" style={{border: "15px", }} role="group" aria-label="Vertical button group">
+  
+  <button id='red' type="button" 
+  onClick={() => setColor("red")}
+  style={{background: "#F32316",
+  marginBottom:"15px",
+  boxShadow: color === "red" ? "2px 2px 10px 10px #F3544A" : "none",}} >
+
+  </button>
+  <button id='yellow' type="button" 
+  onClick={() => setColor("yellow")}
+  style={{background: "#F9E714",
+  marginBottom:"15px",
+  boxShadow: color === "yellow" ? "2px 2px 10px 10px #F5E638" : "none",}}>
+
+  </button>
+  <button id='green' type="button" 
+  onClick={() => setColor("green")}
+  style={{background: "#1BBB1D",
+  boxShadow: color === "green" ? "2px 2px 10px 10px #2CBF2E" : "none",}}>
+
+  </button>
+</div>  
+    </div>)
+}
+
+
+export default TrafficLight;
